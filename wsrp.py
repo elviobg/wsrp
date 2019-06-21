128b3b921a8a611d4142ac8486d3e9de2bd8b8b1
Elvio Gomes <elviobg@gmail.com>
2019-06-21 00:30:43 -0300

    Adiciona funções para importar e exportar grafos
    a pasta de destino está fixa

--- wsrp.py
+++ wsrp.py
@@ -1,44 +1,52 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 from queue import deque
+import json
 
 def mount_graph():
-    """
-    theGraph = nx.DiGraph()
-    theGraph.add_node('a1', time=1)
-    theGraph.add_node('b1', time=1)
-    theGraph.add_node('b2', time=1)
-    theGraph.add_node('c1', time=1)
-    theGraph.add_edge('a1','b1')
-    theGraph.add_edge('a1','b2')
-    theGraph.add_edge('b1','c1')
     
-    theGraph.add_node(7, time=1)
-    theGraph.add_node(5, time=1)
-    theGraph.add_node(6, time=1)
-    theGraph.add_edge(5,6)
-    theGraph.add_edge(5,7)
-    """
     theGraph = nx.DiGraph()
-    theGraph.add_node('5', time=1)
-    theGraph.add_node('7', time=1)
-    theGraph.add_node('3', time=1)
-    theGraph.add_node('11', time=1)
-    theGraph.add_node('8', time=1)
-    theGraph.add_node('2', time=1)
-    theGraph.add_node('9', time=1)
-    theGraph.add_node('10', time=1)
+    theGraph.add_node('pa1', time=1, service='pedreiro')
+    theGraph.add_node('pb1', time=1, service='pedreiro')
+    theGraph.add_node('pb2', time=1, service='pedreiro')
+
+    theGraph.add_node('ea1', time=1, service='encanador')
+    theGraph.add_node('ea2', time=1, service='encanador')
+    theGraph.add_node('eb1', time=1, service='encanador')
+    theGraph.add_node('eb2', time=1, service='encanador')
+    theGraph.add_node('eb3', time=1, service='encanador')
+
+    theGraph.add_node('la1', time=1, service='eletricista')
+    theGraph.add_node('la2', time=1, service='eletricista')
+    theGraph.add_node('lb1', time=1, service='eletricista')
+    theGraph.add_node('lb2', time=1, service='eletricista')
+    theGraph.add_node('lb3', time=1, service='eletricista')
     
-    theGraph.add_edge('5','11')
-    theGraph.add_edge('11','2')
-    theGraph.add_edge('7','11')
-    theGraph.add_edge('7','8')
-    theGraph.add_edge('3','8')
-    theGraph.add_edge('11','9')
-    theGraph.add_edge('11','10')
-    theGraph.add_edge('3','10')
+    theGraph.add_edge('pa1','ea1')
+    theGraph.add_edge('pa1','ea2')
+    theGraph.add_edge('ea1','la1')
+    theGraph.add_edge('ea2','la2')
+
+    theGraph.add_edge('pb1','pb2')
+    theGraph.add_edge('pb1','eb1')
+    theGraph.add_edge('pb1','eb2')
+    theGraph.add_edge('pb2','eb3')
+    theGraph.add_edge('eb1','lb1')
+    theGraph.add_edge('eb2','lb2')
+    theGraph.add_edge('eb3','lb3')
 
     return theGraph
+
+def export_graph(theGraph, name):
+    data = nx.json_graph.node_link_data(theGraph)
+    with open("graphs\\" + name + ".txt", 'w+') as outfile:  
+        json.dump(data, outfile)
+
+def import_graph(name):
+    with open("graphs\\" + name + ".txt") as json_file:  
+        data = json.load(json_file)
+        H = nx.json_graph.node_link_graph(data)
+        return H
 
 def plot_graph(theGraph):
     nx.draw(theGraph)
@@ -52,21 +60,41 @@ def topological_sort(theGraph):
     topSort = []
     q = deque() 
 
-    for vertex in theGraph.in_degree:
-        if vertex[1] == 0:
-            q.appendleft(vertex[0])
+    for edge in theGraph.in_degree:
+        if edge[1] == 0:
+            q.appendleft(edge[0])
     
     while q:
-        vertex = q.pop()
-        topSort.append(vertex)
-        for adj in theGraph.adj[vertex]:
+        node = q.pop()
+        topSort.append(node)
+        for adj in theGraph.adj[node]:
             if adj not in q:
                 q.appendleft(adj)
     
     print(topSort)    
     return topSort
 
+def show(theGraph, order):
+    for vertex in order:
+        current = theGraph.node[vertex]
+        print(current)
+
+def generate_wsrp_matrix(theGraph, order):
+    n_nodes = len(theGraph)
+    for i in range(n_nodes):
+        print('--------------------')
+        print(theGraph.nodes[order[i]])
+        for j in range(i, n_nodes):
+            print('\t', theGraph.nodes[order[j]])
+
 if __name__ == '__main__':
-    theGraph = mount_graph()
+
+    #theGraph = mount_graph()
+    #export_graph(theGraph, 'exemplo3')
+    
+    theGraph = import_graph('exemplo3')
     plot_graph(theGraph)
-    topological_sort(theGraph)
+    order = topological_sort(theGraph)
+    show(theGraph, order)
+    #generate_wsrp_matrix(theGraph, order)
+    
